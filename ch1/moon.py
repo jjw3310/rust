9783088744b105c4a1cea79325477bc9ee567e3e
likejjw <jjw3310@gmail.com>
2023-05-25 07:29:50 +0900

    ch1

---
+++ ch1/moon.py
@@ -0,0 +1,5 @@
+moon = 384400
+car = 80
+btrain = 300
+print("달까지 자동차로 {}일".format(moon/car/24))
+print("달까지 자동차로 {}일".format(moon/btrain/24))
---
+++ ch1/show_price.py
@@ -0,0 +1,2 @@
+banana = 300
+print("바나나 가격={}원".format(banana))
